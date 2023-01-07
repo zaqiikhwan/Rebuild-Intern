670a254ccb740eda1c9a6e20760ed9fa784c44ea
Muh Zaqi <zaqiikhwan22@gmail.com>
2023-01-07 18:05:15 +0700

    refactor cors

--- cors/cors.go
+++ cors/cors.go
@@ -4,7 +4,7 @@ import "github.com/gin-gonic/gin"
 
 func CORSPreflightMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://petlink-eta.vercel.app")
 		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, authorization, accept, origin, Cache-Control, X-Requested-With")
--- handler/handler.clinic.go
+++ handler/handler.clinic.go
@@ -67,17 +67,18 @@ func SearchClinicUsingQuery(c *gin.Context) {
 
 	if result := db.Find(&queryResults); result.Error != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"success": false,
-			"message": "Query is not supplied.",
-			"error":   result.Error.Error(),
+			"success"	: false,
+			"message"	: "Query is not supplied.",
+			"error"		: result.Error.Error(),
 		})
 		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"success": true,
-		"message": "Search successful",
-		"data":    queryResults,
-		"search": location,
+		"success"	: true,
+		"message"	: "Search successful",
+		"data"		: queryResults,
+		"length"	: len(queryResults),
+		"search"	: location,
 	})
 }
