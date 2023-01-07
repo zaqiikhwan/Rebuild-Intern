76bc6de29eb95d15ce8f69f1f8fe392661779977
Muh Zaqi <zaqiikhwan22@gmail.com>
2023-01-07 18:12:38 +0700

    refactor cors

--- routers/routers.main.go
+++ routers/routers.main.go
@@ -2,18 +2,29 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
-	"main.go/cors"
 )
 
 func InitGin() *gin.Engine {
 	app := gin.Default()
 	app.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello World!",
-			"Text":    "Hello world",
+			"message": "success",
+			"text":    "connected",
 		})
 	})
-	app.Use(cors.CORSPreflightMiddleware())
+	app.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		if c.Request.Method == "OPTIONS" {
+			c.Writer.Header().Set("Content-Type", "application/json")
+			c.AbortWithStatus(204)
+		} else {
+			c.Next()
+		}
+	})
 	ArticleDomainRouter(app)
 	ClinicDomainRouter(app)
 	DoctorDomainRouter(app)
